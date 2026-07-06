23eaf5b938f6c5c4dbaeb930b89175492e7ada50
Marcosdeleprani <marcos_deleprani@estudante.sesisenai.org.br>
2026-07-06 15:54:24 -0300

    Adição: ProdutoDAO-AvaliacaoDAO

--- weganosDB/src/dao/AvaliacaoDAO.java
+++ weganosDB/src/dao/AvaliacaoDAO.java
@@ -1,5 +1,99 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import conexao.Conexao;
+import model.Avaliacao;
+import model.Produto;
+
 public class AvaliacaoDAO {
-    
+    public void salvar(Avaliacao avaliacao) {
+        String sql = "INSERT INTO AVALIACAO (nota_avaliacao, desc_avaliacao, FK_PRODUTO_id_produto) VALUES (?, ?, ?)";
+
+        try (Connection conn = Conexao.getConexao();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, avaliacao.getNotaAvaliacao());
+            stmt.setObject(2, avaliacao.getDescAvaliacao());
+            stmt.setInt(3, avaliacao.getFkProdutoIdProduto().getIdProduto());
+
+            stmt.executeUpdate();
+            System.out.println("Sucesso: Avaliação salva no banco de dados!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao salvar Avaliação: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<Avaliacao> listarTodos() {
+        String sql = "SELECT * FROM AVALIACAO";
+        ArrayList<Avaliacao> lista = new ArrayList<>();
+
+        try (Connection conn = Conexao.getConexao();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Avaliacao avaliacao = new Avaliacao();
+
+                avaliacao.setIdAvaliacao(rs.getInt("id_avaliacao"));
+                avaliacao.setNotaAvaliacao(rs.getInt("nota_avaliacao"));
+                avaliacao.setDescAvaliacao(rs.getString("desc_avaliacao"));
+                
+                Produto produto = new Produto();
+                produto.setIdProduto(rs.getInt("FK_PRODUTO_id_produto"));
+                avaliacao.setFkProdutoIdProduto(produto);
+                lista.add(avaliacao);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar Avaliação: " + e.getMessage());
+        }
+
+        return lista;
+    }
+
+    public void alterar(Avaliacao avaliacao) {
+        String sql = "UPDATE AVALIACAO SET nota_avaliacao = ?, desc_avaliacao = ?, FK_PRODUTO_id_produto = ?  WHERE id_avaliacao = ?";
+
+        try (Connection conn = Conexao.getConexao();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, avaliacao.getNotaAvaliacao());
+            stmt.setString(2, avaliacao.getDescAvaliacao());
+            stmt.setInt(3, avaliacao.getFkProdutoIdProduto().getIdProduto());
+
+            stmt.executeUpdate();
+            System.out.println("Avaliação atualizado com sucesso no banco!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao alterar Avaliação: " + e.getMessage());
+        }
+    }
+
+    public void deletar(Avaliacao avaliacao) {
+
+        String sql = "DELETE FROM AVALIACAO WHERE id_avaliacao = ?";
+
+        try (Connection conn = Conexao.getConexao();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, avaliacao.getIdAvaliacao());
+
+            int linhasAfetadas = stmt.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Sucesso: Avaliação deletada com sucesso!");
+            } else {
+                System.out.println("Aviso: Nenhuma Avaliação encontrado com o ID informado.");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao deletar Avaliação: " + e.getMessage());
+        }
+    }
 }
--- weganosDB/src/model/Produto.java
+++ weganosDB/src/model/Produto.java
@@ -1,6 +1,6 @@
 package model;
 import java.math.BigDecimal;
-class Produto {
+public class Produto {
     private int idProduto;
     private String nomeProduto;
     private BigDecimal precoProduto;
@@ -8,7 +8,84 @@ class Produto {
     private String descProduto;
     private BigDecimal valorCompraProduto;
     private BigDecimal valorVendaProduto;
-    private int fkFornecedorIdFornecedor;
-    private int fkCategoriaIdCategoria;
+    private Fornecedor fkFornecedorIdFornecedor;
+    private Categoria fkCategoriaIdCategoria;
 
+
+     public Produto() {
+    }
+
+    public void mostrarProduto(){
+         System.out.println("=======================================");
+        System.out.println("ID: "+idProduto + "\nNome: "+nomeProduto+ "\nPreço: "+precoProduto+"\nQuantidade Estoque: "+qtdEstoqueProduto+"\nDescrição: "+descProduto+"\nValor Compra: "+valorCompraProduto+"\nValor Venda: "+valorVendaProduto+"\nID Fornecedor: "+fkFornecedorIdFornecedor+"\nID Categoria: "+fkCategoriaIdCategoria);
+        System.out.println("=======================================");
+    }
+    public int getIdProduto() {
+        return idProduto;
+    }
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+    public BigDecimal getPrecoProduto() {
+        return precoProduto;
+    }
+    public void setPrecoProduto(BigDecimal precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+    public int getQtdEstoqueProduto() {
+        return qtdEstoqueProduto;
+    }
+    public void setQtdEstoqueProduto(int qtdEstoqueProduto) {
+        this.qtdEstoqueProduto = qtdEstoqueProduto;
+    }
+    public String getDescProduto() {
+        return descProduto;
+    }
+    public void setDescProduto(String descProduto) {
+        this.descProduto = descProduto;
+    }
+    public BigDecimal getValorCompraProduto() {
+        return valorCompraProduto;
+    }
+    public void setValorCompraProduto(BigDecimal valorCompraProduto) {
+        this.valorCompraProduto = valorCompraProduto;
+    }
+    public BigDecimal getValorVendaProduto() {
+        return valorVendaProduto;
+    }
+    public void setValorVendaProduto(BigDecimal valorVendaProduto) {
+        this.valorVendaProduto = valorVendaProduto;
+    }
+    public Fornecedor getFkFornecedorIdFornecedor() {
+        return fkFornecedorIdFornecedor;
+    }
+    public void setFkFornecedorIdFornecedor(Fornecedor fkFornecedorIdFornecedor) {
+        this.fkFornecedorIdFornecedor = fkFornecedorIdFornecedor;
+    }
+    public Categoria getFkCategoriaIdCategoria() {
+        return fkCategoriaIdCategoria;
+    }
+    public void setFkCategoriaIdCategoria(Categoria fkCategoriaIdCategoria) {
+        this.fkCategoriaIdCategoria = fkCategoriaIdCategoria;
+    }
+
+    public Produto(int idProduto, String nomeProduto, BigDecimal precoProduto, int qtdEstoqueProduto,
+            String descProduto, BigDecimal valorCompraProduto, BigDecimal valorVendaProduto,
+            Fornecedor fkFornecedorIdFornecedor, Categoria fkCategoriaIdCategoria) {
+        this.idProduto = idProduto;
+        this.nomeProduto = nomeProduto;
+        this.precoProduto = precoProduto;
+        this.qtdEstoqueProduto = qtdEstoqueProduto;
+        this.descProduto = descProduto;
+        this.valorCompraProduto = valorCompraProduto;
+        this.valorVendaProduto = valorVendaProduto;
+        this.fkFornecedorIdFornecedor = fkFornecedorIdFornecedor;
+        this.fkCategoriaIdCategoria = fkCategoriaIdCategoria;
+    }
 }
