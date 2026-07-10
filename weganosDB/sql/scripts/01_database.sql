853b0cf2fbdbd4d100238a1a438cf8c4dfa9d835
ALANNIS BEATRIZ LANGE <alannis_lange@estudante.sesisenai.org.br>
2026-07-10 19:10:05 -0300

    Resolução de conflitos

--- weganosDB/sql/scripts/01_database.sql
+++ weganosDB/sql/scripts/01_database.sql
@@ -1,29 +1,3 @@
 USE WeganosDB;
 DROP DATABASE WeganosDB;
 CREATE DATABASE WeganosDB;
-
---NINGUEM PODE USAR
-lock tables CLIENTE READ;
-lock tables CATEGORIA READ;
-lock tables PRODUTO READ;
-lock tables PEDIDO READ;
-lock tables AVALIACAO READ;
-lock tables TRANSPORTADORA READ;
-lock tables LOGIN READ;
-lock tables CARRINHO_CONTEM READ;
-lock tables PAGAMENTO READ;
-lock tables FORNECEDOR READ;
-
---EU POSSO USAR O BANCO/TABELA
-lock tables CATEGORIA write;
-lock tables PRODUTO write;
-lock tables PEDIDO write;
-lock tables AVALIACAO write;
-lock tables TRANSPORTADORA write;
-lock tables LOGIN write;
-lock tables CARRINHO_CONTEM write;
-lock tables PAGAMENTO write;
-lock tables FORNECEDOR write;
-
---LIBERA TUDO
-unlock tables ;
