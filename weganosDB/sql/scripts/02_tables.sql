77ac110e9fc6d8a39218302c5d36ee601593e878
ALANNIS BEATRIZ LANGE <alannis_lange@estudante.sesisenai.org.br>
2026-08-04 18:40:09 -0300

    Adição file permissoes

--- weganosDB/sql/scripts/02_tables.sql
+++ weganosDB/sql/scripts/02_tables.sql
@@ -88,13 +88,3 @@ CREATE TABLE LOGIN (
     senha_login varchar(100) not null
 );
 
-
-CREATE TRIGGER senha_apos_login BEFORE INSERT ON LOGIN 
-FOR EACH ROW BEGIN
-    SET NEW.senha_login = SHA2(NEW.senha_login, 256);
-END;
-
-CREATE TRIGGER senha_apos_update BEFORE UPDATE ON LOGIN 
-FOR EACH ROW BEGIN
-    SET NEW.senha_login = SHA2(NEW.senha_login, 256);
-END;
